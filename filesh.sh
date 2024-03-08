20dc4e7ea0b120b0813636a09a3fd676d3d80718
Narasimhulu T. -X (nat - ALLEGIS SERVICES INDIA PRIVATE LIMITED at Cisco) <nat@cisco.com>
2024-03-08 17:33:37 +0530

    files changed

--- filesh.sh
+++ filesh.sh
@@ -4,7 +4,7 @@ count=0
 git log --pretty=format:"%h" origin/main..HEAD | while IFS= read -r commit_hash; do
     echo "Commit: $commit_hash"
     git diff --stat $commit_hash^ $commit_hash
-    ((count++))
+    count=$((count+1))
 done
 
 echo "Total number of modified files: $count"
