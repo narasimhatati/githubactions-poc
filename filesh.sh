edc29e8cd012fd191118c065162a62cf01c9c1a6
Narasimhulu T. -X (nat - ALLEGIS SERVICES INDIA PRIVATE LIMITED at Cisco) <nat@cisco.com>
2024-03-08 17:52:20 +0530

    files changed

--- filesh.sh
+++ filesh.sh
@@ -3,7 +3,9 @@
 count=0
 git log --pretty=format:"%h" origin/main..HEAD | while IFS= read -r commit_hash; do
     echo "Commit: $commit_hash"
-    git diff --stat $commit_hash^ $commit_hash
+    git show --stat --color=always $commit_hash
+    git show --color=always $commit_hash
+    # git diff --stat $commit_hash^ $commit_hash
     count=$((count+1))
 done
 
