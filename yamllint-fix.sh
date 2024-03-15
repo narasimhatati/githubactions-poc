f8d4f9b854a6cbfa10187ef062bf7eecc018ecfe
Narasimhulu T. -X (nat - ALLEGIS SERVICES INDIA PRIVATE LIMITED at Cisco) <nat@cisco.com>
2024-03-15 08:05:07 +0530

    automate yaml lint errors

--- yamllint-fix.sh
+++ yamllint-fix.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
-
-# Run yamllint to check YAML files and capture linting errors
 lint_output=$(yamllint -c .yamllint ./**/*.yaml ./**/*.yml)
-
-# Check if there are any linting errors
 if [ -n "$lint_output" ]; then
-    # Fix indentation issues using yq
     find . -name "*.yaml" -o -name "*.yml" | xargs -I {} sh -c 'yq eval "." -i {}'
-
-    # Remove trailing spaces using yq
     find . -name "*.yaml" -o -name "*.yml" | xargs -I {} sh -c 'yq eval "del(. *.*  | select(type == \"string\" and test(\"\\s\$\")))" -i {}'
 fi
