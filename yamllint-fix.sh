de5a6c05c7e9aef6c469a8a2312ce27d37621f72
Narasimhulu T. -X (nat - ALLEGIS SERVICES INDIA PRIVATE LIMITED at Cisco) <nat@cisco.com>
2024-03-15 08:13:06 +0530

    automate yaml lint errors

--- yamllint-fix.sh
+++ yamllint-fix.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
 lint_output=$(yamllint -c .yamllint ./**/*.yaml ./**/*.yml)
+
 if [ -n "$lint_output" ]; then
-    find . -name "*.yaml" -o -name "*.yml" | xargs -I {} sh -c 'yq eval "." -i {}'
-    find . -name "*.yaml" -o -name "*.yml" | xargs -I {} sh -c 'yq eval "del(. *.*  | select(type == \"string\" and test(\"\\s\$\")))" -i {}'
+    while IFS= read -r file; do
+        yq eval "." -i "$file"
+        yq eval "del(. *.*  | select(type == \"string\" and test(\"\\s\$\")))" -i "$file"
+    done < <(find . -name "*.yaml" -o -name "*.yml")
 fi
